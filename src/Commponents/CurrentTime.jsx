49a96ecbed1e9a774dbe5b9e9cdbcdc64837b879
being-shivalika <shivalika271@gmail.com>
2025-12-30 15:40:51 +0530

    .

--- src/Commponents/CurrentTime.jsx
+++ src/Commponents/CurrentTime.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 let CurrentTime = () => {
-  let time = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("interval has been setup");
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      console.log("cancelled an interval ");
+    };
+  }, []);
+
   return (
     <p className="lead">
       This is the current time: {time.toLocaleDateString()}-
       {time.toLocaleTimeString()}
     </p>
-    
   );
 };
 export default CurrentTime;
